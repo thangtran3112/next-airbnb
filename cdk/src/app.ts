b0cf7019ca5b56d984c0db113bc95798877e41b7
Thang Tran <trathanl@amazon.com>
2024-06-29 22:16:26 -0700

    refactor and add deployment notes

--- cdk/src/app.ts
+++ cdk/src/app.ts
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 import * as cdk from "aws-cdk-lib";
 import { AppRouterStack } from "./app-router-stack";
+import { OverridesStack } from "./overrides-stack";
 
 const app = new cdk.App();
-new AppRouterStack(app, "app-css"); // ar = app router
+new AppRouterStack(app, "app-airbnb"); // ar = app router
